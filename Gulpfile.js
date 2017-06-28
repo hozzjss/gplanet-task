31a3c67020a7494768c78982a85b3498d229d9a2
hozzjss <medo3578@gmail.com>
2017-06-28 17:16:29 +0200

    workflow setup

---
+++ Gulpfile.js
@@ -0,0 +1,46 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+const input = './main.scss';
+const output = '.';
+
+const sassOptions = {
+  errLogToConsole: true,
+  outputStyle: 'expanded'
+};
+
+const autoprefixerOptions = {
+  browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
+};
+
+gulp.task('sass', () => {
+  return gulp
+    .src(input)
+    .pipe(sass(sassOptions).on('error', sass.logError))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(output))
+});
+
+gulp.task('watch', function() {
+  return gulp
+    // Watch the input folder for change,
+    // and run `sass` task when something happens
+    .watch(input, ['sass'])
+    // When there is a change,
+    // log a message in the console
+    .on('change', (event) => {
+      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+    });
+});
+
+
+gulp.task('prod', ['watch'], () => {
+  return gulp
+    .src(input)
+    .pipe(sass({ outputStyle: 'compressed' }))
+    .pipe(autoprefixer(autoprefixerOptions))
+    .pipe(gulp.dest(output));
+});
+
+gulp.task('default', ['sass', 'watch']);
