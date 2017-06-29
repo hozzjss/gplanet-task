cb169927e0d2ec99d9aa52b3504f431192a0368f
hozzjss <medo3578@gmail.com>
2017-06-29 02:29:15 +0200

    Header finished

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
-const input = './scss/main.scss';
+const input = './scss/**/*.scss';
 const output = './css';
 
 const sassOptions = {
