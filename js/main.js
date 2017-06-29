738c942e960eeb041290914a119f1edd2f92a598
hozzjss <medo3578@gmail.com>
2017-06-30 01:33:10 +0200

    finished everything but footer

--- js/main.js
+++ js/main.js
@@ -1,26 +1,40 @@
 'use strict';
 
 $(document).ready(function () {
+    var stars = 3;
+
     var generateNewStar = function generateNewStar(dataRating) {
         return '<svg height="25" width="23" class="star rating" data-rating="' + dataRating + '">\n                    <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;" />\n                </svg>';
     };
+
     var fiveStars = '';
     for (var i = 1; i < 6; i += 1) {
         fiveStars += generateNewStar(i);
     }
+
+    var fiveStillStars = '';
+    for (var _i = 1; _i < 6; _i += 1) {
+        if (_i > stars) {
+            fiveStillStars += '<img src="./assets/Empty_Star.png">';
+        } else {
+            fiveStillStars += '<img src="./assets/Gold_Star.png">';
+        }
+    }
     var generateNewUser = function generateNewUser() {
         var name = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : 'Alexander Kallaway';
         var imgSrc = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : './assets/user.jpg';
-        var stars = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : '1';
+        var stars = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : stars;
+        var active = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : "";
 
-        return '\n        <a class="list-group-item" href="#">\n            <div class="user">\n                <div class="img">\n                    <img class="img-responsive" src="' + imgSrc + '" alt="">\n                </div>\n                <div class="review-excerpt">\n                    <h5>Super Good</h5>\n                    <div class="stars" data-stars="' + stars + '">\n                        ' + fiveStars + '\n                    </div>\n                    <div class="name">\n                        <h6>' + name + '</h6>\n                    </div>\n                    <div class="message-excerpt">\n                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam odit mollitia, architecto enim autem ipsa earum cupiditate\n                        itaque odio nam.\n                    </div>\n                </div>\n            </div>\n        </a>';
+        return '\n        <a class="list-group-item ' + active + '" href="#">\n            <div class="user">\n                <div class="img">\n                    <img class="img-responsive" src="' + imgSrc + '" alt="">\n                </div>\n                <div class="review-excerpt">\n                    <h5>Super Good</h5>\n                    <div class="still-stars">\n                        ' + fiveStillStars + '\n                    </div>\n                    <div class="name">\n                        <h6>' + name + '</h6>\n                    </div>\n                    <div class="message-excerpt">\n                        Lorem ipsum dolor sit amet, consectetur adipisicing...\n                    </div>\n                </div>\n            </div>\n        </a>';
     };
-    var userList = $('.user-list.list-group');
+    var userList = $('.user-list .list-group');
     // add five stars to chat-info
     $('.chat-info .stars').append(fiveStars);
 
     // add five users
-    for (var _i = 0; _i < 5; _i += 1) {
+    var active = true;
+    for (var _i2 = 0; _i2 < 5; _i2 += 1) {
         userList.append(generateNewUser());
     }
     $('.star.rating').click(function () {
--- main.js
+++ main.js
@@ -1,45 +1,58 @@
 $(document).ready(function () {
+    const stars = 3;
+
     const generateNewStar = (dataRating) => {
         return `<svg height="25" width="23" class="star rating" data-rating="${dataRating}">
                     <polygon points="9.9, 1.1, 3.3, 21.78, 19.8, 8.58, 0, 8.58, 16.5, 21.78" style="fill-rule:nonzero;" />
                 </svg>`
     }
+
     let fiveStars = '';
     for (let i = 1; i < 6; i += 1) {
         fiveStars += generateNewStar(i);
     }
+
+    let fiveStillStars = '';
+    for (let i = 1; i < 6; i += 1) {
+        if (i > stars) {
+            fiveStillStars += '<img src="./assets/Empty_Star.png">'
+        } else {
+            fiveStillStars += '<img src="./assets/Gold_Star.png">'
+        }
+    }
     const generateNewUser = (
         name = 'Alexander Kallaway',
         imgSrc = './assets/user.jpg',
-        stars = '1'
+        stars = stars,
+        active = ""
     ) => {
         return `
-        <a class="list-group-item" href="#">
+        <a class="list-group-item ${active}" href="#">
             <div class="user">
                 <div class="img">
                     <img class="img-responsive" src="${imgSrc}" alt="">
                 </div>
                 <div class="review-excerpt">
                     <h5>Super Good</h5>
-                    <div class="stars" data-stars="${stars}">
-                        ${fiveStars}
+                    <div class="still-stars">
+                        ${fiveStillStars}
                     </div>
                     <div class="name">
                         <h6>${name}</h6>
                     </div>
                     <div class="message-excerpt">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam odit mollitia, architecto enim autem ipsa earum cupiditate
-                        itaque odio nam.
+                        Lorem ipsum dolor sit amet, consectetur adipisicing...
                     </div>
                 </div>
             </div>
         </a>`
     };
-    const userList = $('.user-list.list-group');
+    const userList = $('.user-list .list-group');
     // add five stars to chat-info
     $('.chat-info .stars').append(fiveStars);
 
     // add five users
+    let active = true;
     for (let i = 0; i < 5; i += 1) {
         userList.append(generateNewUser())
     }
